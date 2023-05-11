2afb5ea4387c53274599ce1dd03387bb17f18093
Yorick Aban <yorickaban@gmail.com>
2023-05-11 15:35:28 -0700

    Implement api calls and edit function

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,95 @@
+import { useState, useEffect } from "react";
+
+function Profile() {
+  const [userData, setUserData] = useState({});
+  const [tempData, setTempData] = useState({});
+  const [editing, setEditing] = useState(false);
+
+  // Fetch the user data from the API on load
+  useEffect(() => {
+    const getUserData = async () => {
+      const userID = 'user_id'
+      try {
+        // Fetch the user data from the API
+        const response = await fetch(`/api/users/${userId}`);
+        if (response.ok) {
+          // Convert the response to JSON
+          const data = await response.json();
+          // Save the user data to state
+          setUserData(data);
+          setTempData(data);
+        } else {
+          console.error("Something went wrong!");
+        }
+      } catch (error) {
+        console.error(`Error fetching data: ${error}`);
+      }
+    };
+
+    getUserData();
+  }, []);
+
+  const handleEditing = () => {
+    setEditing(!editing);
+  };
+
+  const handleSave = async (e) => {
+    e.preventDefault();
+
+    const userId = 'user_id';
+    try {
+      // Save the data to the API
+      const response = await fetch(`/api/users/${userId}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(tempData),
+      });
+
+      if (response.ok) {
+        // Convert the response to JSON
+        const data = await response.json();
+        // Update the user data
+        setUserData(data);
+        setEditing(false);
+      } else {
+        console.error("Something went wrong!");
+      }
+    } catch (error) {
+      console.error(`Error updating data: ${error}`);
+    }
+  };
+
+  return (
+    <div>
+      <img src={userData.profilePicture} alt="Profile Picture" />
+      <div>{userData.userID}</div>
+      <div>
+        {editing ? (
+          <input
+            value={tempData.name}
+            onChange={(e) => setTempData({ ...tempData, name: e.target.value })}
+          />
+        ) : (
+          userData.name
+        )}
+      </div>
+      <div>
+        {editing ? (
+          <textarea
+            value={tempData.bio}
+            onChange={(e) => setTempData({ ...tempData, bio: e.target.value })}
+          />
+        ) : (
+          userData.bio
+        )}
+      </div>
+      {editing ? (
+        <button onClick={handleSave}>Save</button>
+      ) : (
+        <button onClick={handleEdit}>Edit</button>
+      )}
+    </div>
+  );
+}
+
+export default Profile;
