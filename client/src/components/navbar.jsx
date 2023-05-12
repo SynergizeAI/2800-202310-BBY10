2570c8192631cedfbafa4c760610c5ec2687aded
Seung Hoon (Jason) Lee <122334139+jlee1095@users.noreply.github.com>
2023-05-12 15:18:10 -0700

    Revert "Revert "a lot""
    This reverts commit fdfa13e0ec232e1d44ba27b8fe5e2fc3626b2f7d.

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,41 +1,37 @@
 import React from "react";
  
 // We import bootstrap to make our application look better.
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
  
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
  
 // Here, we display our Navbar
-export default function Navbar() {
- return (
-   <div>
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <NavLink className="navbar-brand" to="/">
-       <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
-       </NavLink>
-       <button
-         className="navbar-toggler"
-         type="button"
-         data-toggle="collapse"
-         data-target="#navbarSupportedContent"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle navigation"
-       >
-         <span className="navbar-toggler-icon"></span>
-       </button>
- 
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto">
-           <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Create Record
-             </NavLink>
-           </li>
-         </ul>
-       </div>
-     </nav>
-   </div>
- );
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <NavLink exact to="/" className="navbar-brand">Home</NavLink>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <NavLink to="/login" className="nav-link" activeClassName="active">Login</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/signup" className="nav-link" activeClassName="active">Sign Up</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/profile" className="nav-link" activeClassName="active">Profile</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/logout" className="nav-link" activeClassName="active">Logout</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
+
+export default Navbar;
