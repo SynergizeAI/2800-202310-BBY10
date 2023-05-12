cc9c495ce7d3799e29a55b40b994d51aa2755159
Kevin Liang <kevinliang0303@gmail.com>
2023-05-12 10:48:42 -0700

    Implement group chat api

--- server/controllers/chatSpaceController.mjs
+++ server/controllers/chatSpaceController.mjs
@@ -1,38 +1,114 @@
-import ChatSpace from '../models/chatSpace.mjs';
+import db from "../db/conn.mjs";
+import generateUniqueCode from "../utils/generateUniqueCode.mjs";
+import getUserIdFromToken from "../utils/getUserIdFromToken.js";
+import { ObjectId } from "mongodb";
 
 export const createChatSpace = async (req, res) => {
   try {
-    const { title } = req.body;
-    const newChatSpace = new ChatSpace({
-      title: title,
-      code: Math.random().toString(36).substr(2, 5), // generate a random 5 character string
-      members: [req.session.userId], // add the user who created the chat space as a member
-    });
+    const userId = getUserIdFromToken(req.cookies.token);
 
-    const chatSpace = await newChatSpace.save();
-    res.status(201).json(chatSpace);
+    const { title } = req.body;
+    const newChatSpace = {
+      title: title,
+      code: await generateUniqueCode(5), // generate a random 5 character string
+      members: [userId], // add the user who created the chat space as a member
+    };
+
+    const collection = await db.collection("chatSpaces");
+
+    const result = await collection.insertOne(newChatSpace);
+
+    const insertedId = await collection.findOne({ _id: result.insertedId });
+    res.status(201).json(insertedId); // ops contains the documents inserted with added _id fields
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: error.message });
   }
 };
 
 export const joinChatSpace = async (req, res) => {
   try {
+    const userId = getUserIdFromToken(req.cookies.token);
     const { code } = req.body;
-    const chatSpace = await ChatSpace.findOne({ code: code });
+    const chatSpace = await db.collection("chatSpaces").findOne({ code: code });
 
     if (!chatSpace) {
-      return res.status(404).json({ message: 'Chat space not found.' });
+      return res.status(404).json({ message: "Chat space not found." });
     }
 
     // add the user to the chat space members if they're not already a member
-    if (!chatSpace.members.includes(req.session.userId)) {
-      chatSpace.members.push(req.session.userId);
-      await chatSpace.save();
+    if (!chatSpace.members.includes(userId)) {
+      const updatedChatSpace = await db
+        .collection("chatSpaces")
+        .findOneAndUpdate(
+          { code: code },
+          { $push: { members: userId } },
+          { returnOriginal: false } // This option instructs the method to return the document after updates were applied.
+        );
+
+      return res.json(updatedChatSpace.value);
     }
 
     res.json(chatSpace);
   } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getChatSpace = async (req, res) => {
+  try {
+    const userId = getUserIdFromToken(req.cookies.token);
+    const { id } = req.params;
+
+    const collection = await db.collection("chatSpaces");
+
+    const chatSpace = await collection.findOne({ _id: new ObjectId(id) });
+
+    if (!chatSpace) {
+      return res.status(404).json({ message: "Chat space not found." });
+    }
+
+    if (!chatSpace.members.includes(userId)) {
+      return res.status(403).json({ message: "Forbidden." });
+    }
+
+    const members = await db
+      .collection("users")
+      .find({ userId: { $in: chatSpace.members } })
+      .toArray();
+
+    const populatedChatSpace = { ...chatSpace, members };
+
+    res.json(populatedChatSpace);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const validateMembership = async (req, res) => {
+  console.log("validateMembership");
+  try {
+    const { chatSpaceId } = req.query;
+    const userId = getUserIdFromToken(req.cookies.token);
+
+    const collection = await db.collection("chatSpaces");
+    const chatSpace = await collection.findOne({
+      _id: new ObjectId(chatSpaceId),
+    }); // Wrap chatSpaceId with ObjectId()
+    console.log(chatSpace);
+
+    if (chatSpace.members.includes(userId)) {
+      return res
+        .status(200)
+        .json({ message: "User is a member of this chat space." });
+    } else {
+      return res
+        .status(403)
+        .json({ message: "User is not a member of this chat space." });
+    }
+  } catch (error) {
+    console.log(error);
     res.status(500).json({ message: error.message });
   }
 };
--- server/controllers/messageController.mjs
+++ server/controllers/messageController.mjs
@@ -1,18 +1,44 @@
-import Message from '../models/message.mjs';
+import db from "../db/conn.mjs";
+import getUserIdFromToken from "../utils/getUserIdFromToken.js";
 
 export const sendMessage = async (req, res) => {
+  const userId = getUserIdFromToken(req.cookies.token);
   try {
     const { chatSpaceId, text } = req.body;
-    const newMessage = new Message({
-      user: req.session.userId,
+    const newMessage = {
+      user: userId,
       chatSpace: chatSpaceId,
       text: text,
       timestamp: new Date(),
-    });
+    };
 
-    const message = await newMessage.save();
-    res.status(201).json(message);
+    const collection = await db.collection("messages");
+
+    const result = await collection.insertOne(newMessage);
+
+    const insertedId = await collection.findOne({ _id: result.insertedId });
+
+    res.status(201).json(insertedId); // ops contains the documents inserted with added _id fields
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getMessages = async (req, res) => {
+  try {
+    const { chatSpaceId } = req.params;
+
+    const collection = await db.collection("messages");
+
+    const messages = await collection
+      .find({ chatSpace: chatSpaceId })
+      .sort({ timestamp: 1 }) // Sort messages by timestamp in ascending order
+      .toArray();
+
+    res.json(messages);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
