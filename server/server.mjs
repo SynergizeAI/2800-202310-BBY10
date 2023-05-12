e4628ad0ac69e0defc55c156b2cb102bbc4643e7
Kevin Liang <kevinliang0303@gmail.com>
2023-05-12 10:49:46 -0700

    Update routes and add ably conn

--- server/server.mjs
+++ server/server.mjs
@@ -1,25 +1,48 @@
-import express from 'express';
-import cors from 'cors';
-import './loadEnvironment.mjs';
-import records from './routes/record.mjs';
-import logins from './routes/login.mjs';
-import signup from './routes/signup.mjs';
-import users from './routes/user.mjs';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import records from "./routes/record.mjs";
+import logins from "./routes/login.mjs";
+import signup from "./routes/signup.mjs";
+import users from "./routes/user.mjs";
+import getUsers from "./routes/getUsers.mjs";
+import { connectAbly } from "./ably.mjs";
+// import userRoutes from './routes/userRoutes.mjs';
+import chatSpaceRoutes from "./routes/chatSpaceRoutes.mjs";
+import messageRoutes from "./routes/messageRoutes.mjs";
+import ablyAuth from "./routes/ablyAuth.mjs";
 
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
-import cookieParser from 'cookie-parser';
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cookieParser());  // This line is important!
+try {
+  console.log("Connecting to Ably...");
+  await connectAbly();
+} catch (error) {
+  console.error("Failed to connect to Ably:", error);
+}
+
+app.use(cookieParser()); // This line is important!
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Update this to your frontend URL
+  origin: "http://localhost:5173", // Update this to your frontend URL
   credentials: true, // This allows cookies to be sent with requests
-  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  methods: ["GET", "POST", "PATCH", "DELETE"],
 };
+
+// mongoose.connect(process.env.ATLAS_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// mongoose.connection.once("open", () => {
+//   console.log("MongoDB connection established successfully");
+// });
 
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
@@ -38,20 +61,25 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({
       mongoUrl: process.env.ATLAS_URI,
-      collectionName: 'sessions',
+      collectionName: "sessions",
     }),
     cookie: {
       secure: false, // Change this to true for production with HTTPS
       maxAge: 1000 * 60 * 60, // 1 hour
       httpOnly: true,
     },
-  }),
+  })
 );
 
-app.use('/api/record', records);
-app.use('/api/login', logins);
-app.use('/api/signup', signup);
-app.use('/api/users', users);
+app.use("/api/record", records);
+app.use("/api/login", logins);
+app.use("/api/signup", signup);
+app.use("/api/users", users);
+app.use("/api/getusers", getUsers);
+// app.use('/api/users', userRoutes);
+app.use("/api/spaces", chatSpaceRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/ably-auth", ablyAuth);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
