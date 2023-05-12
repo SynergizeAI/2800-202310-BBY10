c347a5bc3a22b4878564738fa6e3866c0ccc6433
Yorick Aban <yorickaban@gmail.com>
2023-05-12 11:59:52 -0700

    Fix navbar for mobile

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,69 +1,59 @@
 import React from "react";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Navbar() {
+function CustomNavbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <NavLink exact to="/" className="navbar-brand">
-        Home
-      </NavLink>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        onClick={() => console.log(document.querySelector("#navbarNav"))}
-        >
-      
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <NavLink
-              to="/login"
-              className="nav-link"
-              activeClassName="active"
-            >
-              Login
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/signup"
-              className="nav-link"
-              activeClassName="active"
-            >
-              Sign Up
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/profile"
-              className="nav-link"
-              activeClassName="active"
-            >
-              Profile
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/logout"
-              className="nav-link"
-              activeClassName="active"
-            >
-              Logout
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <NavLink exact to="/" className="navbar-brand">
+          Home
+        </NavLink>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <NavLink
+                to="/login"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Login
+              </NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink
+                to="/signup"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Sign Up
+              </NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink
+                to="/profile"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Profile
+              </NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink
+                to="/logout"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Logout
+              </NavLink>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default CustomNavbar;
