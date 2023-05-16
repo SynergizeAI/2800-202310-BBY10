ca7f5dbdbdfdc411d9d83bbf269c04a9a9483109
Yorick Aban <yorickaban@gmail.com>
2023-05-16 15:11:12 -0700

    Fix bugs in file

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,10 @@
-import EmptyState from "@app/components/EmptyState";
+import EmptyState from "../components/EmptyState";
 
 const Users = () => {
     return (
         <div className="hidden lg:block lg:pl-80 h-full">
             <EmptyState />
-        </div>>
+        </div>
     );
 }
 
