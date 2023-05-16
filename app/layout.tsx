c71d6567f3a254da913948c0333fb269461eb547
Seung Hoon (Jason) Lee <122334139+jlee1095@users.noreply.github.com>
2023-05-16 13:56:55 -0700

    Add UI interface layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import AuthContext from "./context/AuthContext";
 import ToasterContext from "./context/ToasterContext";
 import "./globals.css";
 import { Inter } from "next/font/google";
@@ -17,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AuthContext>
         <ToasterContext />
         {children}
+        </AuthContext>
         </body>
     </html>
   );
