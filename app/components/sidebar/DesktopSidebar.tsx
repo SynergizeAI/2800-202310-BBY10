99f51ca3095de556a85bd9baab2c059112da28e7
Yorick Aban <yorickaban@gmail.com>
2023-05-16 15:12:38 -0700

    Add some styling and user functionality

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,8 +1,19 @@
+'use client';
+
 import useRoutes from "@/app/hooks/useRoutes";
+import { User } from "@prisma/client";
 import { useState } from "react";
+import DesktopItem from "./DesktopItem";
+import Avatar from "../Avatar";
 
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+    currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
    const routes = useRoutes();
    const [isOpen, setIsOpen] = useState(false);
 
@@ -54,6 +65,30 @@ const DesktopSidebar = () => {
                 ))}
             </ul>
         </nav>
+        <nav
+            className="
+            mt-4
+            flex
+            flex-col
+            justify-between
+            items-center
+            "
+        >
+            <div
+                onClick={() => setIsOpen(true)}
+                className="
+                
+                cursor-pointer
+                hover:opacity
+                transition
+                "
+                >
+                <Avatar user={currentUser} />
+            </div>
+
+        </nav>
     </div>
    );
 }
+
+export default DesktopSidebar;
