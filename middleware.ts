fb6a8b0f8f487aab339a40d59f2392d1f4bff2f4
Seung Hoon (Jason) Lee <122334139+jlee1095@users.noreply.github.com>
2023-05-17 15:11:37 -0700

    Update middleware and added new pages header and profile drawer

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,23 @@
-import { withAuth } from "next-auth/middleware"
+/**
 
+This file contains a middleware that protects specific routes by
+requiring authentication using NextAuth.
+Unauthenticated users will be redirected to the sign-in page.
+*/
+// Import the withAuth middleware from NextAuth
+import { withAuth } from "next-auth/middleware";
+
+// Define and export the middleware configuration
 export default withAuth({
-    pages: {
-        signIn: "/"
-    }
+// Specify the sign-in page path to redirect unauthenticated users
+pages: {
+signIn: "/",
+},
 });
 
+// Additional configuration settings for the middleware
 export const config = {
-    matcher: [
-        "/users/:path*"
-    ]
+// Define an array of URL patterns to match specific routes
+// that require authentication
+matcher: ["/conversations/:path*", "/users/:path*"],
 };
