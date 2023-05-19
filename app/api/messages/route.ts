4a3c71b2e8dd03db2cf51b38ba5cb4f7558d9a24
Kevin Liang <kevinliang0303@gmail.com>
2023-05-18 22:52:16 -0700

    Implement chatbot

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import getCurrentUser from "@/app/_actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
 import { pusherServer } from "@/app/libs/pusher";
+import { upsert } from "./_utils/upsert";
+import { query } from "./_utils/query";
+import { prompt } from "./_utils/prompt";
 
 /**
  * Handles a POST request to create a new message in a conversation.
@@ -47,6 +50,7 @@ export async function POST(request: Request) {
         messages: { include: { seen: true } },
       },
     });
+    console.log(newMessage);
 
     await pusherServer.trigger(conversationId, "messages:new", newMessage);
 
@@ -60,11 +64,127 @@ export async function POST(request: Request) {
       });
     });
 
+    let botResponsePromise;
+    if (message.startsWith("@flo")) {
+      // remove first word
+      const command = message.split(" ").slice(1).join(" ");
+      console.log("command", command);
+
+      // Send a "bot:typing" event to the front end
+      pusherServer.trigger(conversationId, "bot:typing", { isTyping: true });
+
+      // Start generating the bot response, but don't wait for it to finish
+      botResponsePromise = generateBotResponse(
+        command,
+        conversationId,
+        currentUser
+      );
+    }
+
     // No need to get the last message here as we just created it
     // Return the new message as response
-    return NextResponse.json(newMessage);
+    const response = NextResponse.json(newMessage);
+
+    await upsert(message, newMessage.id, conversationId, currentUser.name);
+
+    // If a bot response is being generated, wait for it to finish
+    if (botResponsePromise) {
+      await botResponsePromise;
+    }
+
+    return response;
   } catch (error) {
     console.log("An error occurred when posting a new message: ", error);
     return new NextResponse("Error", { status: 500 });
   }
+}
+
+async function generateBotResponse(
+  command: string,
+  conversationId: string,
+  currentUser: any
+) {
+
+  const matches = await query(command, conversationId);
+  // console.log("context", matches);
+
+  const conversation = await prisma.conversation.findUnique({
+    where: { id: conversationId },
+    include: {
+      users: { select: { name: true } },
+      messages: {
+        select: {
+          body: true,
+          createdAt: true,
+          sender: { select: { name: true } },
+        },
+        orderBy: { createdAt: "desc" },
+        take: 5,
+      },
+    },
+  });
+
+  const name = conversation?.name;
+  const users = conversation?.users.map((user) => user.name);
+
+  const conversation_log = conversation?.messages.map((message) => {
+    const { body, createdAt, sender } = message;
+    return { timestamp: createdAt, sender: sender.name, text: body };
+  });
+
+  const context = matches?.map((match) => match.metadata);
+
+  const context_log = context?.map((log: any) => {
+    const { sender, text, timestamp } = log;
+    return { timestamp, sender, text };
+  });
+
+  const props = {
+    users,
+    name,
+    context_log,
+    conversation_log,
+    command,
+  };
+
+  const botResponse = await prompt(props);
+
+  pusherServer.trigger(conversationId, "bot:typing", { isTyping: false });
+
+  // Create a new message from the bot
+  const botMessage = await prisma.message.create({
+    include: { seen: true, sender: true },
+    data: {
+      body: botResponse,
+      conversation: { connect: { id: conversationId } },
+      sender: { connect: { id: "64648a2b0a9a533e1373b18e" } },
+      seen: { connect: { id: currentUser.id } },
+    },
+  });
+
+  // Update the conversation with the new message and the last message timestamp
+  const botUpdatedConversation = await prisma.conversation.update({
+    where: { id: conversationId },
+    data: {
+      lastMessageAt: new Date(),
+      messages: { connect: { id: botMessage.id } },
+    },
+    include: {
+      users: true,
+      messages: { include: { seen: true } },
+    },
+  });
+
+  // Trigger an update to the conversation with the bot's new message
+  await pusherServer.trigger(conversationId, "messages:new", botMessage);
+
+  // Notify all users of the conversation update
+  botUpdatedConversation.users.map((user) => {
+    pusherServer.trigger(user.email!, "conversation:update", {
+      id: conversationId,
+      messages: [botMessage],
+    });
+  });
+
+  return botMessage;
 }
