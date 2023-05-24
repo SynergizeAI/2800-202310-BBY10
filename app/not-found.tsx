07c15597d84c763fa16a75cca63f4277e780e581
Seung Hoon (Jason) Lee <122334139+jlee1095@users.noreply.github.com>
2023-05-24 15:55:10 -0700

    Create 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className='text-center w-screen h-screen flex flex-col justify-center items-center'>
+        <h1 className='text-9xl font-bold'>404</h1>
+        <p>Could not find requested resource</p>
+      </div>
+    );
+  }
